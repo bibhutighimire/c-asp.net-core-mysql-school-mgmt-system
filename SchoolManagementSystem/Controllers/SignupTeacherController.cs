0051c1f2ebe71eb87124d06dd851b27925162f23
bibhutighimire <ghimirebibhuti@gmail.com>
2020-10-03 18:54:41 -0600

    list of course names can be seen in dropdown of signupteacher controller page now

---
+++ SchoolManagementSystem/Controllers/SignupTeacherController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SchoolManagementSystem.Models;
+
+namespace SchoolManagementSystem.Controllers
+{
+    public class SignupTeacherController : Controller
+    {
+        private readonly ConnectionDB _context;
+
+        public SignupTeacherController(ConnectionDB context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var fromDatabaseEF = new SelectList(_context.tblCoursename.ToList(), "Coursename");
+            ViewData["Coursename"] = fromDatabaseEF;
+            List<Teacher> ListOfTeachers = _context.tblTeacher.ToList();
+            //ViewBag.Listofteacher = ListOfTeachers;
+            List<Coursename> listofcoursename = _context.tblCoursename.ToList();
+            ViewBag.listofcoursename = _context.tblCoursename.ToList();
+            var joinedtable = from t in ListOfTeachers
+                              join c in listofcoursename on t.teacherid equals c.teacherid
+                              select new NewVM { listofcoursename = c, ListOfTeachers = t };
+
+            return View(joinedtable);
+        }
+        //[HttpPost]
+        //public IActionResult Index(Teacher teacher)
+        //{
+        //    Teacher t = new Teacher();
+        //    t.firstname = teacher.firstname;
+        //    t.lastname = teacher.lastname;
+        //    t.dob = teacher.dob;
+        //    t.email = teacher.email;
+        //    t.positionid = 3;
+        //    t.username = teacher.username;
+        //    t.password = teacher.password;
+        //    t.coursenameid = _context.tblCoursename.Select(x=>x.coursenameid==teacher.);
+
+        //    return View();
+        //}
+    }
+}
---
+++ SchoolManagementSystem/Models/Admin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Models
+{
+	[Table("tblAdmin")]
+	public class Admin
+    {
+		[Display(Name = "Admin ID")]
+		public int adminid { get; set; }
+		[Required(ErrorMessage = "First Name Required")]
+		[Display(Name = "First Name")]
+		public string firstname { get; set; }
+		[Required(ErrorMessage = "Last Name Required")]
+		[Display(Name = "Last Name")]
+		public string lastname { get; set; }
+		[Required(ErrorMessage = "DOB Required")]
+		[Display(Name = "Date of Birth")]
+		[DataType(DataType.Date)]
+		public DateTime dob { get; set; }
+		[Required(ErrorMessage = "Email Required")]
+		[Display(Name = "Email")]
+		[DataType(DataType.EmailAddress)]
+		public string email { get; set; }
+		public int positionid { get; set; }
+		[Required(ErrorMessage = "Username Required")]
+		[Display(Name = "Username")]
+		public string username { get; set; }
+		[Required(ErrorMessage = "Password Required")]
+		[Display(Name = "Password")]
+		[DataType(DataType.Password)]
+		public string password { get; set; }
+		
+	}
+}
